26c0cfba9449c16854b7a99c1f4f261af9e07a27
PaiLeora <fanatiksirpai@gmail.com>
2026-06-12 01:19:16 +0700

    Create firebaseconfig.js

---
+++ public/src/Services/firebaseconfig.js
@@ -0,0 +1,9 @@
+// Konfigurasi Firebase - GANTI DENGAN PUNYA LO!
+export const firebaseConfig = {
+  apiKey: "AIzaSyBc40PhCjvxQvr3GqactJd3zkZqELhW0VA",
+  authDomain: "cybernoir-game.firebaseapp.com",
+  projectId: "cybernoir-game",
+  storageBucket: "cybernoir-game.firebasestorage.app",
+  messagingSenderId: "1093030267928",
+  appId: "1:1093030267928:web:52fffcadc4a35118c9076d"
+}
