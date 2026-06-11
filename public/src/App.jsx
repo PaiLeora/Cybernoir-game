978cae876cd5679b5ce07a6500d68aaa05585a54
PaiLeora <fanatiksirpai@gmail.com>
2026-06-12 01:52:34 +0700

    Update App.jsx

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,34 +1,19 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { useAuth } from './contexts/AuthContext'
-import LoadingScreen from './components/LoadingScreen'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import Profile from './pages/Profile'
-import Tutorial from './pages/Tutorial'
-import CaseInvestigation from './pages/CaseInvestigation'
-import EvidenceBoard from './pages/EvidenceBoard'
-
-const PrivateRoute = ({ children }) => {
-  const { user, loading } = useAuth()
-  if (loading) return <LoadingScreen />
-  return user ? children : <Navigate to="/login" />
-}
+import './index.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#0a0a0f]">
+    <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-        <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-        <Route path="/tutorial" element={<PrivateRoute><Tutorial /></PrivateRoute>} />
-        <Route path="/case/:caseId" element={<PrivateRoute><CaseInvestigation /></PrivateRoute>} />
-        <Route path="/evidence/:caseId" element={<PrivateRoute><EvidenceBoard /></PrivateRoute>} />
       </Routes>
-    </div>
+    </BrowserRouter>
   )
 }
 
