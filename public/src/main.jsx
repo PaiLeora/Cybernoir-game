4d664ba72b87ccded484157dcef6e58934dcd11b
PaiLeora <fanatiksirpai@gmail.com>
2026-06-12 00:17:01 +0700

    Update main.jsx

--- public/src/main.jsx
+++ public/src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
@@ -13,25 +11,14 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <DndProvider backend={HTML5Backend}>
-        <AuthProvider>
-          <GameProvider>
-            <SoundProvider>
-              <App />
-              <Toaster 
-                position="top-right"
-                toastOptions={{
-                  style: {
-                    background: '#1a1a2e',
-                    color: '#00d4ff',
-                    border: '1px solid #00d4ff',
-                  },
-                }}
-              />
-            </SoundProvider>
-          </GameProvider>
-        </AuthProvider>
-      </DndProvider>
+      <AuthProvider>
+        <GameProvider>
+          <SoundProvider>
+            <App />
+            <Toaster position="top-right" />
+          </SoundProvider>
+        </GameProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+))
