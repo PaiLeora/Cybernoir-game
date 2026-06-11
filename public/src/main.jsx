3660801e096a87e4ab28b480ee3afb3c7b33198b
PaiLeora <fanatiksirpai@gmail.com>
2026-06-12 01:53:23 +0700

    Update main.jsx

--- public/src/main.jsx
+++ public/src/main.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
 import App from './App'
-import { AuthProvider } from './contexts/AuthContext'
-import { GameProvider } from './contexts/GameContext'
-import { SoundProvider } from './contexts/SoundContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <GameProvider>
-          <SoundProvider>
-            <App />
-            <Toaster position="top-right" />
-          </SoundProvider>
-        </GameProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
