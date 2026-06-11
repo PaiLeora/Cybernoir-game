a46fa49105446a9bf89fe322c34e97dfd386e4a5
PaiLeora <fanatiksirpai@gmail.com>
2026-06-12 00:07:55 +0700

    Create CaseInvestigation.jsx

---
+++ public/src/pages/CaseInvestigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+
+const CaseInvestigation = () => {
+  const { caseId } = useParams()
+  
+  return (
+    <div className="min-h-screen bg-[#0a0a0f]">
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <div className="cyber-card text-center">
+          <h1 className="text-2xl font-cyber text-[#00d4ff]">Case #{caseId}</h1>
+          <p className="text-gray-400 mt-4">Investigation tools coming soon...</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CaseInvestigation
